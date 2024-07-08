1545b3a8175aa2c0c217f050210e668a8dd81558
Ms-moraveji1973 <ms.moraveji2007@gmail.com>
2024-07-08 16:35:33 +0330

    fixed bug

--- mafia/views.py
+++ mafia/views.py
@@ -13,8 +13,9 @@ def mafia(request):
             goodman_player = 1
             matador_player = 1
             grup_mafia = goodman_player + matador_player + godfather_player
-            if (num_players - grup_mafia ) > 11 :
+            if num_players > 11 :
                 mafia_players = int((num_players / 3) - grup_mafia)
+                print(mafia_players)
             else :
                 mafia_players = 0
             ken_player = 1
@@ -22,28 +23,28 @@ def mafia(request):
             doctor_player = 1
             constantine_player = 1
             nostradamus_player = 1
-            Citizenshipـroles = ken_player + leon_player + doctor_player + constantine_player   
+            Citizenshipـroles = ken_player + leon_player + doctor_player + constantine_player +  nostradamus_player
             Mafias_roles = godfather_player + matador_player + matador_player + mafia_players
             if num_players - Citizenshipـroles - Mafias_roles  != 0 :
                 citizen_player = int(num_players -Citizenshipـroles - Mafias_roles )
+                print(citizen_player)
             else:
                 citizen_player = 0
             players = []
             for i in range(num_players):
                 players.append(MafiaRoleModel())
+            mafia_role = ['Mafia'] * mafia_players
+            shuffle(mafia_role)
             godfather_role = ['GodFather'] * godfather_player
             goodman_role = ['GoodMan'] * goodman_player
             matador_role = ['Matador'] * matador_player
-            mafia_role = ['Mafia'] * mafia_players
-            shuffle(mafia_role)
             ken_role = ['Ken'] * ken_player
             leon_role = ['Leon'] * leon_player
             doctor_role = ['Doctor'] * doctor_player
             constantine_role = ['Constantine'] * constantine_player
             nostradamus_role = ['Nostradamus'] * 1
             citizen_role = ['Citizen'] * citizen_player 
-            all_role = mafia_role + citizen_role + leon_role + doctor_role + constantine_role + nostradamus_role + ken_role\
-                        + godfather_role + goodman_role + matador_role  
+            all_role = mafia_role + citizen_role + leon_role + doctor_role + constantine_role + nostradamus_role + ken_role + godfather_role + goodman_role + matador_role  
             shuffle(all_role)
             for i in range(num_players):
                 players[i].name = f"Player {i+1}"
