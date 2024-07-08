509cacf113c60772368f277b0cf961c7249ab6a4
Ms-moraveji1973 <ms.moraveji2007@gmail.com>
2024-07-08 16:19:50 +0330

    Improving the design and adding some roles

--- mafia/models.py
+++ mafia/models.py
@@ -4,10 +4,8 @@ from multiselectfield import MultiSelectField
 # Create your models here.
 
 class MafiaRoleModel(models.Model):
-    name = models.CharField(max_length=50)
-    role = models.CharField(max_length=100, choices=(('godfather', 'GodFather'),('citizen', 'Citizen') ,
-                             ('doctor' , 'Doctor') , ('leon' , 'Leon'), ('nostradamus','Nostradamus') ,
-                             ('constantine' , 'Constantine') , ('ken' , 'Ken') ))
+    name = models.CharField(max_length=50,null=True)
+    role = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name + " (" + self.role + ")"
--- mafia/views.py
+++ mafia/views.py
@@ -9,42 +9,49 @@ def mafia(request):
         form = NumberForm(request.POST)
         if form.is_valid() :
             num_players = form.cleaned_data['mafia_player']
-            mafia_players = int(num_players / 3)
+            godfather_player = 1
+            goodman_player = 1
+            matador_player = 1
+            grup_mafia = goodman_player + matador_player + godfather_player
+            if (num_players - grup_mafia ) > 11 :
+                mafia_players = int((num_players / 3) - grup_mafia)
+            else :
+                mafia_players = 0
             ken_player = 1
             leon_player = 1 
             doctor_player = 1
             constantine_player = 1
             nostradamus_player = 1
-            if num_players-mafia_players-ken_player-leon_player-doctor_player-constantine_player-nostradamus_player != 0 :
-                citizen_player = int(num_players-mafia_players-ken_player-leon_player-doctor_player-constantine_player-nostradamus_player)
+            Citizenshipـroles = ken_player + leon_player + doctor_player + constantine_player   
+            Mafias_roles = godfather_player + matador_player + matador_player + mafia_players
+            if num_players - Citizenshipـroles - Mafias_roles  != 0 :
+                citizen_player = int(num_players -Citizenshipـroles - Mafias_roles )
             else:
                 citizen_player = 0
             players = []
             for i in range(num_players):
                 players.append(MafiaRoleModel())
-
-            mafia_role = ['mafia'] * mafia_players
+            godfather_role = ['GodFather'] * godfather_player
+            goodman_role = ['GoodMan'] * goodman_player
+            matador_role = ['Matador'] * matador_player
+            mafia_role = ['Mafia'] * mafia_players
             shuffle(mafia_role)
-            ken_role = ['ken'] * 1
-            leon_role = ['leon_player'] * 1
-            doctor_role = ['doctor_player'] * 1
-            constantine_role = ['constantine_player'] * 1
-            ken_role = ['ken'] * 1
-            nostradamus_role = ['nostradamus_player'] * 1
-            citizen_role = ['citizen'] * citizen_player 
-            all_role = mafia_role + citizen_role + leon_role + doctor_role + constantine_role + nostradamus_role + ken_role   
+            ken_role = ['Ken'] * ken_player
+            leon_role = ['Leon'] * leon_player
+            doctor_role = ['Doctor'] * doctor_player
+            constantine_role = ['Constantine'] * constantine_player
+            nostradamus_role = ['Nostradamus'] * 1
+            citizen_role = ['Citizen'] * citizen_player 
+            all_role = mafia_role + citizen_role + leon_role + doctor_role + constantine_role + nostradamus_role + ken_role\
+                        + godfather_role + goodman_role + matador_role  
             shuffle(all_role)
             for i in range(num_players):
                 players[i].name = f"Player {i+1}"
                 players[i].role = all_role[i]
-
-            for player in players :
-                player.save()
         
             return render(request , 'game.html' , {'players':players})
         else :
             return render(request , 'home.html' , {'form':form})
-
     else:
         form = NumberForm()
         return render(request ,'home.html' , {'form':form})
@@ -65,5 +72,8 @@ def cart(request):
     for i in range (0,len(carts)) :
         carts[i].cart_id = f"Number{i+1}"
         carts[i].cart = all_carts[i]
+
+    for cart in carts :
+        cart.save()
     return render(request , 'cart.html' , {'carts' : carts})
 
