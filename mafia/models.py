29f4b5f1830c2d7b50a3c800dbd97d45566c2ec0
Ms-moraveji1973 <ms.moraveji2007@gmail.com>
2024-07-08 03:33:37 +0330

    add feature

--- mafia/models.py
+++ mafia/models.py
@@ -4,12 +4,13 @@ from multiselectfield import MultiSelectField
 # Create your models here.
 
 class MafiaRoleModel(models.Model):
-    name = models.CharField(max_length=50 , null=True)
-    role = models.CharField(max_length=10, choices=(('mafia', 'Mafia'), ('citizen', 'Citizen')))
+    name = models.CharField(max_length=50)
+    role = models.CharField(max_length=100, choices=(('godfather', 'GodFather'),('citizen', 'Citizen') ,
+                             ('doctor' , 'Doctor') , ('leon' , 'Leon'), ('nostradamus','Nostradamus') ,
+                             ('constantine' , 'Constantine') , ('ken' , 'Ken') ))
 
     def __str__(self):
         return self.name + " (" + self.role + ")"
-
 
 
 class Player(models.Model):
--- mafia/views.py
+++ mafia/views.py
@@ -7,16 +7,31 @@ def mafia(request):
     if request.method == 'POST':
         num_players = int(request.POST['mafia_player'])
         mafia_players = int(num_players / 3)
-        citizen_player = int(num_players-mafia_players)
-
+        ken_player = 1
+        leon_player = 1
+        doctor_player = 1
+        constantine_player = 1
+        nostradamus_player = 1
+        if num_players-mafia_players-ken_player-leon_player-doctor_player-constantine_player-nostradamus_player != 0 :
+            citizen_player = int(num_players-mafia_players-ken_player-leon_player-doctor_player-constantine_player-nostradamus_player)
+        else:
+            citizen_player = 0
         players = []
         for i in range(num_players):
             players.append(MafiaRoleModel())
 
+        print(players)
+
         mafia_role = ['mafia'] * mafia_players
         shuffle(mafia_role)
+        ken_role = ['ken'] * 1
+        leon_player = ['leon_player'] * 1
+        doctor_player = ['doctor_player'] * 1
+        constantine_player = ['constantine_player'] * 1
+        ken_role = ['ken'] * 1
+        nostradamus_player = ['nostradamus_player'] * 1
         citizen_role = ['citizen'] * citizen_player 
-        all_role = mafia_role + citizen_role
+        all_role = mafia_role + citizen_role + leon_player + doctor_player + constantine_player + nostradamus_player + ken_role   
         shuffle(all_role)
         for i in range(num_players):
             players[i].name = f"Player {i+1}"
